c078fd9caff608711d266a872029028a29e90d9c
NoriSte <nori.ste.magni@gmail.com>
2019-04-16 10:02:25 +0200

    fix: 🐛 Fix a test with the right assertion

--- test-projects/base-configuration-test/cypress/integration/iubenda.spec.js
+++ test-projects/base-configuration-test/cypress/integration/iubenda.spec.js
@@ -3,7 +3,7 @@
 context('Iubenda', () => {
   it('The Iubenda cookie footer should appear', () => {
     cy.visit('http://localhost:9000')
-    cy.get("#iubenda-cs-banner").should("be.exist");
-    cy.get(".iubenda-cs-close-btn").should("be.exist");
+    cy.get("#iubenda-cs-banner").should("exist");
+    cy.get(".iubenda-cs-close-btn").should("exist");
   })
 })

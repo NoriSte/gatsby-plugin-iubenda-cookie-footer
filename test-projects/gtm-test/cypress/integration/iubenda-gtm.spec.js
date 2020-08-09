2175720a0f097cedec9c1a2c0f1a2049e287e3be
Stefano Magni <nori.ste.magni@gmail.com>
2020-08-09 15:42:54 +0200

    fix: fix plugin

--- test-projects/gtm-test/cypress/integration/iubenda-gtm.spec.js
+++ test-projects/gtm-test/cypress/integration/iubenda-gtm.spec.js
@@ -1,17 +1,20 @@
 /// <reference types="Cypress" />
 
-context('Iubenda GTM', () => {
-  it('The Iubenda cookie footer should push the event to the GTM data layer', () => {
-    cy.visit('/');
-    cy.get("#iubenda-cs-banner").should("exist");
-    cy.get(".iubenda-cs-close-btn").should("exist");
+context("Iubenda GTM", () => {
+  it("The Iubenda cookie footer should push the event to the GTM data layer", () => {
+    cy.visit("/")
+    cy.get("#iubenda-cs-banner").should("exist")
+    cy.get(".iubenda-cs-close-btn").should("exist")
 
-    cy.window().then(win => {
-      win.dataLayer = [];
-    });
+    cy.window().then((win) => {
+      win.dataLayer = []
+    })
 
-    cy.get(".iubenda-cs-close-btn").click();
+    cy.get(".iubenda-cs-close-btn").click()
 
-    cy.window().its('dataLayer').should('contain', { event: "iubenda_consent_given" });
+    cy.window()
+      .its("dataLayer")
+      .its("0")
+      .should('be', { event: "iubenda_consent_given" })
   })
 })

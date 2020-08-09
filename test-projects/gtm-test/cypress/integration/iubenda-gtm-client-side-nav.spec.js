4c68fd84e9659bbb5563f834ef82ae56ad19e5b1
Stefano Magni <nori.ste.magni@gmail.com>
2020-08-09 15:42:54 +0200

    fix: fix plugin

--- test-projects/gtm-test/cypress/integration/iubenda-gtm-client-side-nav.spec.js
+++ test-projects/gtm-test/cypress/integration/iubenda-gtm-client-side-nav.spec.js
@@ -1,7 +1,6 @@
 /// <reference types="Cypress" />
 
 const containConsentGivenEvents = num => dataLayer => {
-  expect(dataLayer).contains({ event: "iubenda_consent_given" });
   expect(dataLayer.filter(member => member.event === "iubenda_consent_given").length).to.be.equal(num);
 }
 
@@ -16,19 +15,13 @@ context('Iubenda GTM client-side navigation', () => {
     });
 
     cy.get(".iubenda-cs-close-btn").click();
-
-    cy.window().its('dataLayer').should('contain', { event: "iubenda_consent_given" });
-
-    // reset dataLayer
-    cy.window().then(win => {
-      win.dataLayer = [];
-    });
-
-    cy.contains('Go to page 2').click();
     cy.window().its('dataLayer').should(containConsentGivenEvents(1));
 
-    cy.contains('Go back to the homepage').click();
+    cy.contains('Go to page 2').click();
     cy.window().its('dataLayer').should(containConsentGivenEvents(2));
+
+    cy.contains('Go back to the homepage').click();
+    cy.window().its('dataLayer').should(containConsentGivenEvents(3));
   })
 
   it('The Iubenda cookie footer should not push the event to the GTM data layer if consent has not been given and the user navigates to a different page', () => {
@@ -36,16 +29,9 @@ context('Iubenda GTM client-side navigation', () => {
     cy.get("#iubenda-cs-banner").should("exist");
     cy.get(".iubenda-cs-close-btn").should("exist");
 
-    // reset dataLayer
-    cy.window().then(win => {
-      win.dataLayer = [];
-    });
-
     cy.contains('Go to page 2').click();
 
-    cy.once("fail", (err) => {
-      expect(err.message).to.be.equal("Timed out retrying: expected [] to include { event: 'iubenda_consent_given' }");
-    })
-    cy.window().its('dataLayer').should('contain', { event: "iubenda_consent_given" }); // we want this to fail, hence the cy.once('fail', ...) above!
+    // the plugin creates the  `dataLayer` global variable at first event
+    cy.window().its('dataLayer').should('be.undefined');
   })
 })

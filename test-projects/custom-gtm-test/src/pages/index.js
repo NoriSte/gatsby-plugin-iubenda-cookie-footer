6a727cb8d4ad90570f3e062cc1f4e4f1b8333e3c
Stefano Magni <nori.ste.magni@gmail.com>
2021-10-31 21:19:19 +0100

    chore(deps): update Gatsby to the v4

--- test-projects/custom-gtm-test/src/pages/index.js
+++ test-projects/custom-gtm-test/src/pages/index.js
@@ -3,11 +3,11 @@ import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Seo from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <Seo title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
@@ -15,13 +15,15 @@ const IndexPage = () => (
       src="../images/gatsby-astronaut.png"
       width={300}
       quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
+      formats={["auto", "webp", "avif"]}
       alt="A Gatsby astronaut"
       style={{ marginBottom: `1.45rem` }}
     />
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
+      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
+      <Link to="/using-dsg">Go to "Using DSG"</Link>
     </p>
   </Layout>
 )
